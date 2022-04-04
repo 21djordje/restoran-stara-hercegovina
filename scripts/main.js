7b62172a91447ba44d776b568f49088401298ffe
djordje.sinikovic <djordjesinikovic@gmail.com>
2022-04-04 02:54:03 +0200

    first version

---
+++ scripts/main.js
@@ -0,0 +1,78 @@
+
+$('.gallery-interijer').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled:true,
+      navigateByImgClick: true,
+      tPrev: 'Previous (Left arrow key)', // title for left button
+      tNext: 'Next (Right arrow key)', // title for right button
+    }
+  });
+
+  $('.gallery-eksterijer').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled:true,
+      navigateByImgClick: true,
+      tPrev: 'Previous (Left arrow key)', // title for left button
+      tNext: 'Next (Right arrow key)', // title for right button
+    }
+  });
+
+  $('.gallery-sala').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled:true,
+      navigateByImgClick: true,
+      tPrev: 'Previous (Left arrow key)', // title for left button
+      tNext: 'Next (Right arrow key)', // title for right button
+    }
+  });
+
+
+  $('.gallery-apartman-1').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled:true,
+      navigateByImgClick: true,
+      tPrev: 'Previous (Left arrow key)', // title for left button
+      tNext: 'Next (Right arrow key)', // title for right button
+    }
+  });
+
+  $('.gallery-apartman-2').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled:true,
+      navigateByImgClick: true,
+      tPrev: 'Previous (Left arrow key)', // title for left button
+      tNext: 'Next (Right arrow key)', // title for right button
+    }
+  });
+
+  $('.gallery-apartman-3').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled:true,
+      navigateByImgClick: true,
+      tPrev: 'Previous (Left arrow key)', // title for left button
+      tNext: 'Next (Right arrow key)', // title for right button
+    }
+  });
+
+  $('.gallery-apartman-4').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled:true,
+      navigateByImgClick: true,
+      tPrev: 'Previous (Left arrow key)', // title for left button
+      tNext: 'Next (Right arrow key)', // title for right button
+    }
+  });
